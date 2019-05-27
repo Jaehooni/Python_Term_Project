a60cad0c51e523de0abc8a70ce92de2bd959f03b
장재훈 <jay4221@khu.ac.kr>
2019-05-27 20:16:25 +0900

    Add Naver_News_Crawling_Tool.ipynb and Daum_News_Crawling_Tool.ipynb and Data file

--- Result.py
+++
@@ -1,8 +0,0 @@
-import datetime
-from News_finder import news_Finder
-start = datetime.date(2019,1,5)
-end = datetime.datetime.now()
-search_Sentences = ['버닝썬', '장자연']
-for search_Sentence in search_Sentences:
-    file_Name = search_Sentences
-    news_Finder(search_Sentence,start,end,file_Name)
